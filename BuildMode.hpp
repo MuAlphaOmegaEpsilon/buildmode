b2bd7fa0c35a176c2ae337bd31cb85ea2787bd90
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-11-14 18:36:49 +0100

    Refactored something

--- BuildMode.hpp
+++ BuildMode.hpp
@@ -2,27 +2,32 @@
 #define BUILDMODE
 #pragma once
 
-/// An enumeration based on boolean values that provides only two possible scenarios: Debug and Release.
-/// There is no need to have multiple build mode scenarios, since cases like MinSizeRel falls under one
-/// of the specified ones (Release in this particular case).
-enum class BuildMode : bool
+namespace BuildMode
 {
-	Release = false,
-	Debug = true
-};
+	/// An enumeration based on boolean values that provides only two possible scenarios: Debug and Release.
+	/// There is no need to have multiple build mode scenarios, since cases like MinSizeRel falls under one
+	/// of the specified ones (Release in this particular case).
+	enum class Mode : bool
+	{
+		Release = false,
+		Debug = true
+	};
 
-#ifdef NDEBUG 	
-/// Specifies at compile time which kind of build mode has been selected.
-/// The setting is taken from the definition of a particular macro, named NDEBUG.
-/// This constexpr value should be used in conjunction with constexpr-if statements.
-constexpr BuildMode currentBuildMode = BuildMode::Release;
+	#ifdef NDEBUG 	
+	/// Specifies at compile time which kind of build mode has been selected.
+	/// The setting is taken from the definition of a particular macro, named NDEBUG.
+	/// This constexpr value should be used in conjunction with constexpr-if statements.
+	constexpr Mode current = Mode::Release;
 
-#else
-/// Specifies at compile time which kind of build mode has been selected.
-/// The setting is taken from the definition of a particular macro, named NDEBUG.
-/// This constexpr value should be used in conjunction with constexpr-if statements.
-constexpr BuildMode currentBuildMode = BuildMode::Debug;
+	#else
+	/// Specifies at compile time which kind of build mode has been selected.
+	/// The setting is taken from the definition of a particular macro, named NDEBUG.
+	/// This constexpr value should be used in conjunction with constexpr-if statements.
+	constexpr Mode current = Mode::Debug;
 
-#endif
+	#endif
+}
+
+
 
 #endif
