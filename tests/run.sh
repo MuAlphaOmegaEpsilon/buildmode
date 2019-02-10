208c60361d9fcd62122c2dd6a0934d33e65e49c2
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-02-10 01:04:45 +0100

    Try to fix travis errors

--- tests/run.sh
+++ tests/run.sh
@@ -6,27 +6,25 @@ cd "$(dirname "${BASH_SOURCE[0]}")" # Navigate to the /tests folder
 ORANGE='\033[0;33m'
 RC='\033[0m' # Remove color
 
-### FUNCTIONS ###
-function testMode
-{
-	mode=$1 # Get the first parameter passed to the function
-	MODE="${mode^^}" # Set every character of the mode to CAPS
-	if [ -d "$mode" ]; then # Remove folder from a previous run
-		rm -r "$mode" 
-	fi
-	mkdir "$mode" # Create directory with the name of the mode
-	cd "$mode" # Go inside the newly created directory
-	echo -e "${ORANGE}TESTING FOR $MODE MODE$RC"
-	cmake ../.. -DTEST=ON -DCMAKE_BUILD_TYPE="$mode" # Create build files
-	make # Build test suite for this mode
-	make test # Start testing
-	result=$? # Save result of the testing
-	cd .. # Go back to the parent directory
-	rm -r "$mode" # Remove the created directory
-	echo # Print a newline
-	return "$result" # Exit with the output of the testing
-}
-
 ### SCRIPT PART ###
-testMode "Debug"
-testMode "Release"
+echo -e "${ORANGE}BUILDING BUILDMODE LIBRARY TESTS$RC"
+if [ -d build ]; then 
+		rm -r build 
+fi
+mkdir build
+
+(
+	cd build
+	cmake ../.. -DTEST=ON
+	echo
+	cat ./CMakeFiles/isDebug.dir/flags.make
+	echo 
+	make VERBOSE=1
+	echo -e "\\n${ORANGE}TESTING BUILDMODE LIBRARY$RC"
+	make test 
+)
+
+result=$?
+rm -r build
+echo 
+exit "$result"
