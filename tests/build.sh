9570ab1b39eb28f5514f9f9562cc8b6fb868c19d
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-06-27 08:35:40 +0200

    Fix CI usage of sonar-scanner (#3)

--- tests/build.sh
+++ tests/build.sh
@@ -9,11 +9,15 @@ ORANGE=$(tput setaf 3)
 NOCOLOR=$(tput sgr0)
 
 ### SCRIPT CORE ###
-printf "%sRUNNING TESTS%s\\n" "${ORANGE}" "${NOCOLOR}"
+printf "%sBUILDING TESTS%s\\n" "${ORANGE}" "${NOCOLOR}"
 if [ ! -d build ]; then
 	printf "\\n%sNo build folder found: maybe you forgot to launch setup.sh first.%s\\n" "${ORANGE}" "${NOCOLOR}"
 	exit 1
 fi
 cd build
 
-ctest -j2
+if [ -n "${SONAR_SCANNER_HOME:-}" ]; then
+	build-wrapper-linux-x86-64 --out-dir ../sonarcloud-dir cmake --build .
+else
+	cmake --build .
+fi
