c65bc8135e115c7f57c939337bdab14fc986ae65
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2019-06-27 08:13:39 +0200

    Mkdir sonarcloud-dir

--- tests/build.sh
+++ tests/build.sh
@@ -17,7 +17,8 @@ fi
 cd build
 
 if [ -n "${SONAR_SCANNER_HOME:-}" ]; then
-	build-wrapper-linux-x86-64 --out-dir sonarcloud-dir cmake --build .;
+	mkdir sonarcloud-dir
+	build-wrapper-linux-x86-64 --out-dir sonarcloud-dir cmake --build .
 else
-	cmake --build .;
+	cmake --build .
 fi
