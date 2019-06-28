85a74daba3ae2235ac4b0a3a6943262a253fb150
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-06-28 08:23:40 +0200

    Update tests reducing dependencies to the bare minimum (#5)

--- tests/test_isDebug.cpp
+++ tests/test_isDebug.cpp
@@ -1,9 +1,8 @@
-#include <stdlib.h>
 #include <buildmode.hpp>
 
 int main ()
 {
 	if (BuildMode::isDebug && !BuildMode::isRelease)
-		return EXIT_SUCCESS;
-	return EXIT_FAILURE;
+		return 0;
+	return 1;
 }
